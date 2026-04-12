aefc38a331b955ac71e2adf04fc3f933ab57df94
suraj-b-17 <surajbalaji.sb@gmail.com>
2026-04-13 02:03:54 +0800

    added StunEffect class to prevent combatant from acting

---
+++ StunEffect.java
@@ -0,0 +1,11 @@
+public class StunEffect extends StatusEffect {
+    public StunEffect() {
+        super(3);
+    }
+
+    @Override
+    public void apply(Combatant target) {}
+
+    @Override
+    public void onTick(Combatant target) {}
+}
