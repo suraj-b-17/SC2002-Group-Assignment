68f8fa54a8b038ee2bee0fea985d11e16103a52f
suraj-b-17 <surajbalaji.sb@gmail.com>
2026-04-18 19:25:02 +0800

    fix smoke bomb message to show block before the damage amount is shown

--- BasicAttackAction.java
+++ BasicAttackAction.java
@@ -13,9 +13,23 @@ public class BasicAttackAction implements Action {
     @Override
     public void execute(BattleEngine battle, Combatant actor) {
         int damage = Math.max(0, actor.getAttack() - target.getDefense());
-        target.takeDamage(damage);
-        System.out.printf("%s attacks %s for %d damage.%n",
-            actor.getName(), target.getName(), damage);
+
+        
+        boolean hasSmokeBomb = target.getStatusEffects().stream()
+            .anyMatch(e -> e instanceof SmokeBombEffect);
+
+        if (hasSmokeBomb) {
+            
+            System.out.printf("%s attempts to attack %s for %d damage, but is blocked by Smoke Bomb!%n",
+                actor.getName(), target.getName(), damage);
+            target.takeDamage(damage); 
+        } else {
+            
+            target.takeDamage(damage);
+            System.out.printf("%s attacks %s for %d damage.%n",
+                actor.getName(), target.getName(), damage);
+        }
+
         if (target.isDefeated()) {
             System.out.printf("%s has been eliminated!%n", target.getName());
         }
